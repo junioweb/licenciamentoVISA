6f02a57513ebba27373cf1e6811bfa92e7f35480
junioweb <junio.webmaster@gmail.com>
2016-02-23 03:51:10 -0300

    Initial Production

---
+++ segCadastro/admin.py
@@ -0,0 +1,166 @@
+# -*- encoding: utf-8 -*-
+
+from django.contrib import admin
+from .models import Responsavel, CBO, Tipo_Servico_Saude
+from .models import Tipo_Equipamento_Saude, Autorizacao_Funcionamento
+from .models import Pessoa_Fisica, Pessoa_Juridica, Equipamento_Saude
+from .models import Atividade, Redesim, Natureza_Juridica
+from .models import Estabelecimento_Desempenha_Atv
+from .models import Assunto_Processo, Processo, Documento, Situacao, Setor
+from .models import Processo_Tramita_Setor, Valor_UFR, Atividade, Categoria_Atividade
+
+class EquipamentoInline(admin.TabularInline):
+    model = Equipamento_Saude
+    extra = 1
+
+class AutorizacaoInline(admin.TabularInline):
+    model = Autorizacao_Funcionamento
+    extra = 1
+
+class DocumentoInline(admin.TabularInline):
+    model = Documento
+    extra = 3
+
+class Estabelecimento_Desempenha_AtvAdmin(admin.ModelAdmin):
+    filter_horizontal = ('ResponsavelTecnico',)
+
+class ProcessoAdmin(admin.ModelAdmin):
+    inlines = [DocumentoInline]
+
+class Pessoa_FisicaAdmin(admin.ModelAdmin):
+    search_fields = ['Nome', 'CPF', 'Municipio']
+    list_display = ('Nome', 'CPF', 'Municipio')
+    filter_horizontal = ('TiposServicoSaude', 'Atividade')
+    fieldsets = [
+        (None,               {'fields': [
+                                        'TipoCadastro', 'Pasta', 'CNES',
+                                        'Nome', 'CPF', 'SiglaConselhoClasse',
+                                        'InscricaoConselhoClasse', 'CEP',
+                                        'Endereco', 'Numero', 'Complemento',
+                                        'PontoReferencia', 'Bairro', 'Municipio',
+                                        'TelefonePrimario', 'TelefoneSecundario',
+                                        'Email', 'Obs',
+                                        ]}),
+        ('Caracterização do Serviço de Saúde', {'fields': ['TiposServicoSaude']}),
+        ('Estabelecimento de Saúde de Natureza Ambulatorial - Informações Estruturais',
+            {'fields': [
+                       'ConsultorioOdontologico', 'Equipo',
+                       'ConsultorioMedicoPediatrico', 'ConsultorioMedicoFeminino',
+                       'ConsultorioMedicoMasculino', 'ConsultorioMedicoOutro',
+                       'OutroConsultorio', 'SalaPediatrico',
+                       'LeitoRepousoPediatrico', 'SalaFeminino',
+                       'LeitoRepousoFeminino', 'SalaMasculino',
+                       'LeitoRepousoMasculino', 'SalaOutro', 'LeitoRepousoOutro',
+                       'SalaPequenaCirurgia', 'SalaNebulizacao', 'SalaGesso',
+                       'SalaImunizacao', 'SalaCurativo', 'SalaEnfermagem',
+                       'SalaCirurgiaAmbulatorial', 'SalaQuimioterapia',
+                       'PoltronaQuimioterapia', 'SalaDialise', 'PoltronaDialise',
+                       ]}),
+        ('Leitos de Estabelecimentos de Saúde com Internação e de Unidades de Urgência / Emergência',
+            {'fields': [
+                       'LtIntCirurgicaBucoMaxiloFacial', 'LtIntCirurgicaCardiologia',
+                       'LtIntCirurgicaCirurgiaGeral', 'LtIntCirurgicaEndocrinologia',
+                       'LtIntCirurgicaGenterologia', 'LtIntCirurgicaGinecologia',
+                       'LtIntCirurgicaLeitoDia', 'LtIntCirurgicaNefroUrologia',
+                       'LtIntCirurgicaNeurocirurgia', 'LtIntCirurgicaObstetricia',
+                       'LtIntCirurgicaOftalmologia', 'LtIntCirurgicaOncologia',
+                       'LtIntCirurgicaOrtTraumatologia', 'LtIntCirurgicaOtorrinolaringo',
+                       'LtIntCirurgicaPlastica', 'LtIntCirurgicaToracica',
+                       'LtIntClinicaAIDS', 'LtIntClinicaCardiologia',
+                       'LtIntClinicaCronicos', 'LtIntClinicaDermatologia',
+                       'LtIntClinicaGeriatria', 'LtIntClinicaHansenologia',
+                       'LtIntClinicaHematologia', 'LtIntClinicaLeitoDia',
+                       'LtIntClinicaNefroUrologia', 'LtIntClinicaNeonatologia',
+                       'LtIntClinicaNeurologia', 'LtIntClinicaObstetricia',
+                       'LtIntClinicaOncologia', 'LtIntClinicaPediatria',
+                       'LtIntClinicaPneumologia', 'LtIntClinicaPsiquiatria',
+                       'LtIntClinicaReabilitacao', 'LtIntClinicaTisiologia',
+                       'UrgIntUTIAdulto', 'UrgIntUTIInfantil',
+                       'UrgIntUTINeonatal', 'UrgIntUTIUnidadeIntermediaria',
+                       'UrgIntUTIUnidadeInterNeonatal', 'UrgIntUTIUnidadeIsolamento',
+                       'UrgLtObservacaoProntoSocorro',
+                       ]}),
+    ]
+    inlines = [EquipamentoInline]
+
+class Pessoa_JuridicaAdmin(admin.ModelAdmin):
+    search_fields = ['RazaoSocial', 'CNPJ', 'Municipio']
+    list_display = ('RazaoSocial', 'CNPJ', 'Municipio')
+    filter_horizontal = ('ResponsaveisLegais', 'TiposServicoSaude', 'ProfissionaisCCIH')
+    fieldsets = [
+        (None,               {'fields': [
+                                        'TipoCadastro', 'Pasta', 'CNES',
+                                        'EstabelecimentoMantenedor',
+                                        'RazaoSocial', 'NomeFantasia', 'CNPJ',
+                                        'NIRE', 'NaturezaJuridica', 'Porte',
+                                        'Tipo', 'CNPJMatriz', 'Natureza',
+                                        'InicioAtividades', 'TerminoAtividades',
+                                        'Situacao', 'ObjetoSocial',
+                                        'AreaUtilizada', 'Metragem',
+                                        'CEP', 'Endereco', 'Numero',
+                                        'Complemento', 'PontoReferencia',
+                                        'Bairro', 'Municipio',
+                                        'TelefonePrimario', 'TelefoneSecundario',
+                                        'Email', 'Obs',
+                                        ]}),
+        ('Responsável(eis) Legal(ais)', {'fields': ['ResponsaveisLegais']}),
+        ('Caracterização do Serviço de Saúde', {'fields': ['TiposServicoSaude']}),
+        ('Estabelecimento de Saúde de Natureza Ambulatorial - Informações Estruturais',
+            {'fields': [
+                       'ConsultorioOdontologico', 'Equipo',
+                       'ConsultorioMedicoPediatrico', 'ConsultorioMedicoFeminino',
+                       'ConsultorioMedicoMasculino', 'ConsultorioMedicoOutro',
+                       'OutroConsultorio', 'SalaPediatrico',
+                       'LeitoRepousoPediatrico', 'SalaFeminino',
+                       'LeitoRepousoFeminino', 'SalaMasculino',
+                       'LeitoRepousoMasculino', 'SalaOutro', 'LeitoRepousoOutro',
+                       'SalaPequenaCirurgia', 'SalaNebulizacao', 'SalaGesso',
+                       'SalaImunizacao', 'SalaCurativo', 'SalaEnfermagem',
+                       'SalaCirurgiaAmbulatorial', 'SalaQuimioterapia',
+                       'PoltronaQuimioterapia', 'SalaDialise', 'PoltronaDialise',
+                       ]}),
+        ('Comissão de Controle de Infecção Hospitalar / CCIH', {'fields': ['ProfissionaisCCIH']}),
+        ('Leitos de Estabelecimentos de Saúde com Internação e de Unidades de Urgência / Emergência',
+            {'fields': [
+                       'LtIntCirurgicaBucoMaxiloFacial', 'LtIntCirurgicaCardiologia',
+                       'LtIntCirurgicaCirurgiaGeral', 'LtIntCirurgicaEndocrinologia',
+                       'LtIntCirurgicaGenterologia', 'LtIntCirurgicaGinecologia',
+                       'LtIntCirurgicaLeitoDia', 'LtIntCirurgicaNefroUrologia',
+                       'LtIntCirurgicaNeurocirurgia', 'LtIntCirurgicaObstetricia',
+                       'LtIntCirurgicaOftalmologia', 'LtIntCirurgicaOncologia',
+                       'LtIntCirurgicaOrtTraumatologia', 'LtIntCirurgicaOtorrinolaringo',
+                       'LtIntCirurgicaPlastica', 'LtIntCirurgicaToracica',
+                       'LtIntClinicaAIDS', 'LtIntClinicaCardiologia',
+                       'LtIntClinicaCronicos', 'LtIntClinicaDermatologia',
+                       'LtIntClinicaGeriatria', 'LtIntClinicaHansenologia',
+                       'LtIntClinicaHematologia', 'LtIntClinicaLeitoDia',
+                       'LtIntClinicaNefroUrologia', 'LtIntClinicaNeonatologia',
+                       'LtIntClinicaNeurologia', 'LtIntClinicaObstetricia',
+                       'LtIntClinicaOncologia', 'LtIntClinicaPediatria',
+                       'LtIntClinicaPneumologia', 'LtIntClinicaPsiquiatria',
+                       'LtIntClinicaReabilitacao', 'LtIntClinicaTisiologia',
+                       'UrgIntUTIAdulto', 'UrgIntUTIInfantil',
+                       'UrgIntUTINeonatal', 'UrgIntUTIUnidadeIntermediaria',
+                       'UrgIntUTIUnidadeInterNeonatal', 'UrgIntUTIUnidadeIsolamento',
+                       'UrgLtObservacaoProntoSocorro',
+                       ]}),
+    ]
+    inlines = [EquipamentoInline, AutorizacaoInline]
+
+admin.site.register(Responsavel)
+admin.site.register(CBO)
+admin.site.register(Tipo_Servico_Saude)
+admin.site.register(Tipo_Equipamento_Saude)
+admin.site.register(Pessoa_Fisica, Pessoa_FisicaAdmin)
+admin.site.register(Pessoa_Juridica, Pessoa_JuridicaAdmin)
+admin.site.register(Estabelecimento_Desempenha_Atv, Estabelecimento_Desempenha_AtvAdmin)
+admin.site.register(Redesim)
+admin.site.register(Processo, ProcessoAdmin)
+admin.site.register(Assunto_Processo)
+admin.site.register(Situacao)
+admin.site.register(Setor)
+admin.site.register(Processo_Tramita_Setor)
+admin.site.register(Valor_UFR)
+admin.site.register(Atividade)
+admin.site.register(Categoria_Atividade)
+admin.site.register(Natureza_Juridica)
---
+++ segCadastro/views.py
@@ -0,0 +1,232 @@
+# -*- encoding: utf-8 -*-
+
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Paragraph, Table, TableStyle, Image
+from reportlab.lib.units import cm, mm, inch, pica
+from reportlab.lib.pagesizes import A4
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+import os
+
+from segCadastro.models import Processo, Estabelecimento, Pessoa_Fisica, Pessoa_Juridica
+from segCadastro.models import Estabelecimento_Desempenha_Atv, Atividade, Processo_Tramita_Setor
+from segCadastro.models import Responsavel
+
+from segCadastro.forms import ProcessoForm, PessoaFisicaForm, PessoaJuridicaForm
+from segCadastro.forms import TramitaSetorForm, Estabelecimento_Desempenha_AtvForm
+from segCadastro.forms import ResponsavelForm
+
+def home(request):
+	return render(request, 'index.html')
+
+def example(request):
+	return render(request, 'example.html')
+
+def estabelecimento(request):
+	return render(request, 'estabelecimento.html')
+
+def processo_listar(request):
+	data = {}
+	data['lista_processos'] = Processo.objects.all()
+	return render(request, 'processo_listar.html', data)
+
+def responsavel_listar(request):
+	data = {}
+	data['lista_responsaveis'] = Responsavel.objects.all()
+	return render(request, 'responsavel_listar.html', data)
+
+def p_fisica_listar(request):
+	data = {}
+	data['lista_p_fisica'] = Pessoa_Fisica.objects.all()
+	return render(request, 'p_fisica_listar.html', data)
+
+def p_juridica_listar(request):
+	data = {}
+	data['lista_p_juridica'] = Pessoa_Juridica.objects.all()
+	return render(request, 'p_juridica_listar.html', data)
+
+def p_fisica_editar(request, pk):
+    p_fisica = Pessoa_Fisica.objects.get(pk=pk)
+
+    form = PessoaFisicaForm(request.POST or None, instance=p_fisica)
+
+    if form.is_valid():
+        form.save()
+        return redirect('p_fisica_listar')
+
+    return render(request, 'p_fisica_editar.html', {'object':p_fisica, 'form':form})
+
+def p_juridica_editar(request, pk):
+    p_juridica = Pessoa_Juridica.objects.get(pk=pk)
+
+    form = PessoaJuridicaForm(request.POST or None, instance=p_juridica)
+
+    if form.is_valid():
+        form.save()
+        return redirect('p_juridica_listar')
+
+    return render(request, 'p_juridica_editar.html', {'object':p_juridica, 'form':form})
+
+def responsavel_editar(request, pk):
+    responsavel = Responsavel.objects.get(pk=pk)
+
+    form = ResponsavelForm(request.POST or None, instance=responsavel)
+
+    if form.is_valid():
+        form.save()
+        return redirect('responsavel_listar')
+
+    return render(request, 'responsavel_editar.html', {'object':responsavel, 'form':form})
+
+def processo_create(request):
+    form = ProcessoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('processo_listar')
+
+    return render(request, 'processo_create.html', {'form':form})
+
+def responsavel_create(request):
+    form = ResponsavelForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('responsavel_listar')
+
+    return render(request, 'responsavel_create.html', {'form':form})
+
+def p_fisica_create(request):
+    form = PessoaFisicaForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('p_fisica_listar')
+
+    return render(request, 'p_fisica_create.html', {'form':form})
+
+def p_juridica_create(request):
+    form = PessoaFisicaForm(None)
+
+    if request.POST:
+        form = PessoaJuridicaForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('p_juridica_listar')
+
+    return render(request, 'p_juridica_create.html', {'form':form})
+
+def estab_atv_vincular(request):
+    form = Estabelecimento_Desempenha_AtvForm(None)
+
+    if request.POST:
+        form = Estabelecimento_Desempenha_AtvForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('p_juridica_listar')
+
+    return render(request, 'estab_atv_vincular.html', {'object':estabelecimento, 'form':form})
+
+def processo_tramitar(request, pk):
+    processo = Processo.objects.get(pk=pk)
+
+    form = TramitaSetorForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('processo_listar')
+
+    return render(request, 'processo_tramitar.html', {'object':processo, 'form':form})
+
+def p_imprimir(request, pk):
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="processo.pdf"'
+
+    #processo_numero = request.GET
+    buffer = BytesIO()
+    processo = Processo.objects.get(pk=pk)
+    tipo = processo.Tipo
+    numero = processo.Numero
+    ano = processo.Ano
+    exercicio = processo.Exercicio
+    estabelecimento = processo.Estabelecimento
+    estab_atividade = processo.Atividade_Estabelecimento.all()
+    processoMae = processo.ProcessoMae
+
+    if processoMae != None:
+		flagProcessoMae = 1
+    else:
+		flagProcessoMae = 0
+
+    processo_numero = tipo+'-'+unicode(numero)+ano+'.'+exercicio+'.'+unicode(flagProcessoMae)
+
+    # Create the PDF object, using the BytesIO object as its "file."
+    p = canvas.Canvas(buffer, pagesize=A4)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    i = os.path.join('/servidorvps/sites/agevisa.tk/htdocs/static/img/topo-A4.jpg')
+    p.drawImage(i, 0, 750, width=21.6*cm, height=2.2*cm)
+    p.setFont("Helvetica", 14)
+    p.drawCentredString(293, 730, u'GOVERNO DO ESTADO DA PARAÍBA')
+    p.drawCentredString(293, 710, u'AGÊNCIA ESTADUAL DE VIGILÂNCIA SANITÁRIA')
+    p.setFont("Helvetica-Bold", 24)
+    p.drawString(40, 660, u'Processo nº: '+processo_numero)
+    p.setFont("Helvetica", 10)
+    p.drawString(40, 640, 'ASSUNTO: '+unicode(processo.Assunto))
+    if processoMae != None:
+		p.drawString(40, 620, u'Processo Mãe: '+unicode(processoMae))
+
+    if estabelecimento != None:
+        p.setFont("Helvetica-Bold", 14)
+        p.drawString(40, 600, 'Dados do Estabelecimento:')
+        p.setFont("Helvetica", 10)
+        pessoa = estabelecimento.child_object()
+
+        if hasattr(pessoa, 'Nome'):
+            p.drawString(40, 580, "Nome: "+pessoa.Nome)
+            p.drawString(40, 540, "CPF: "+pessoa.CPF)
+        elif hasattr(pessoa, 'RazaoSocial'):
+	        p.drawString(40, 580, u"Razão Social: "+pessoa.RazaoSocial)
+	        p.drawString(40, 560, "Nome Fantasia: "+pessoa.NomeFantasia)
+	        p.drawString(40, 540, "CNPJ: "+pessoa.CNPJ)
+
+        p.drawString(40, 520, "CEP: "+estabelecimento.CEP)
+        p.drawString(40, 500, u"Endereço: "+estabelecimento.Endereco)
+        p.drawString(40, 480, u"Número: "+estabelecimento.Numero)
+        p.drawString(40, 460, "Bairro: "+estabelecimento.Bairro)
+        p.drawString(40, 440, u"Município: "+unicode(estabelecimento.Municipio))
+        p.drawString(40, 420, u"Telefone Primário: "+unicode(estabelecimento.TelefonePrimario))
+        if estabelecimento.TelefoneSecundario != 0:
+            p.drawString(40, 400, u"Telefone Secundário: "+unicode(estabelecimento.TelefoneSecundario))
+        p.drawString(40, 380, "E-mail: "+estabelecimento.Email)
+        if processo.Obs != '':
+            p.drawString(40, 360, u"Observação: "+processo.Obs)
+        p.setFont("Helvetica-Bold", 14)
+        p.drawString(40, 320, u'Caracterização da(s) Atividade(s) Econômica(s):')
+        p.setFont("Helvetica", 9)
+
+        i = 320
+        for ar in estab_atividade:
+            atividade = Atividade.objects.get(pk=ar.Atividade_id)
+            i = i-20
+            p.drawString(40, i, atividade.Subclasse+' - '+atividade.Denominacao)
+
+    else:
+        if processo.Obs != '':
+            p.drawString(40, 600, u"Observação: "+processo.Obs)
+
+    # Close the PDF object cleanly.
+    p.showPage()
+    p.save()
+
+    # Get the value of the BytesIO buffer and write it to the response.
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+    return response
