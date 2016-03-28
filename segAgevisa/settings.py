6999f905d4525555c2a2e8526d98fb5165062b66
junioweb <junio.webmaster@gmail.com>
2016-03-28 06:41:34 -0300

    settings

--- segAgevisa/settings.py
+++ segAgevisa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e0w6^97=pa+@bxc&2f04bip638_x%3k&t3*)%)kj-!v&%n+e9e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = [
 	'www.agevisa.tk',
