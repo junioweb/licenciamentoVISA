87a6577cac17524e99abbfd65b58dbb5c2b4703e
Fernando Luis Ferreira da Silva Júnior <fernando.luis@AGEVISA.LOCAL>
2017-02-13 11:42:14 -0300

    PAS

--- segCadastro/views.py
+++ segCadastro/views.py
@@ -259,8 +259,9 @@ def processo_create(request):
         for value in processos:
             if value.Assunto_id == 18:
                 situacao = Processo_Tramita_Setor.objects.filter(Processo_id=request.POST.get('estabelecimento_id')).order_by('-Situacao')[:1]
-                if situacao.Situacao == 'PENA_APL' and request.POST.get('assunto') != 18:
-                    raise ValidationError('Processo não pode ser gerado, pois existe uma penalidade aplicada ao regulado.')
+                for value in situacao:
+                    if value.Situacao == 'PENA_APL' and request.POST.get('assunto') != 18:
+                        raise ValidationError('Processo não pode ser gerado, pois existe uma penalidade aplicada ao regulado.')
 
         if request.POST.get("processo_id"):
             processo.ProcessoMae = Processo.objects.get(pk=request.POST.get("processo_id"))
